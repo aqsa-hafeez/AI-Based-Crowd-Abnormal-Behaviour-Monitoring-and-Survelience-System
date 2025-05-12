ed8f6023e8365a062bed4aa1a3c9f989f0d7cf7d
Aqsa Hafeez <113436185+aqsa-hafeez@users.noreply.github.com>
2025-05-12 14:36:45 +0500

    Add files via upload

---
+++ Front_End/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/muhammad-usama/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/muhammad-usama/Desktop/FYP Frontend/surveilai(5)/surveilai" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/muhammad-usama/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/muhammad-usama/Desktop/FYP Frontend/surveilai(5)/surveilai"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/muhammad-usama/Desktop/FYP Frontend/surveilai(5)/surveilai/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/muhammad-usama/Desktop/FYP Frontend/surveilai(5)/surveilai/lib/main.dart"
+)
